8773087cf4c983935c64a454ba782d631172a2d3
Kibet <nesh@localhost.localdomain>
2024-06-27 10:45:07 +0300

    Ruto must go

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/**
+ * Represents a classroom.
+ */
+export default class ClassRoom {
+  /**
+   * @param - maxStudentsSize - Maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
